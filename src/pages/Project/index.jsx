e853a948b05817543b7d70764714673b9503057f
TomScott <contact@tascott.co.uk>
2023-03-16 20:40:59 +0000

    PW project page

--- src/pages/Project/index.jsx
+++ src/pages/Project/index.jsx
@@ -1,15 +1,29 @@
 import "./style.css";
 import projects from "../../data/projects.json";
 import { useParams, Link } from "react-router-dom";
+import { useState } from "react";
 
 function ProjectPage() {
   const { id } = useParams();
 
-  var selectedProject = projects.find( function( project ){
+  var selectedProject = projects.find(function (project) {
     return project.id === Number(id);
   });
 
-  return (
+  const [isVerified, setIsVerified] = useState(false);
+
+  const checkPw = () => {
+    // gets the current input value
+    const answer = document.getElementById("password").value;
+
+    if (answer === "yourSecretPassword") {
+      setIsVerified(true);
+    } else {
+      alert("Sorry, that's not it");
+    }
+  };
+
+  const YourApp = () => (
     <div className="project">
       <h1>Project ID: {selectedProject.id}</h1>
       <h2>Client: {selectedProject.client_name}</h2>
@@ -20,6 +34,19 @@ function ProjectPage() {
       <p>Estimated Cost: {selectedProject.estimated_costs}</p>
       <Link to={`/projects`}>Back to Project List</Link>
 
+    </div>
+  );
+
+  return (
+    <div>
+      {isVerified ? (
+        <YourApp />
+      ) : (
+        <form onSubmit={checkPw}>
+          <input id="password" name="password" />
+          <button>open sesame</button>
+        </form>
+      )}
     </div>
   );
 }
