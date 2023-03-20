978a9d6bd69e578dd8492b1915d3841ba863d682
Ralph Cox <ralphsmail@btinternet.com>
2023-03-20 21:23:42 +0000

    Updating Routing

---
+++ src/pages/NotFound/index.jsx
@@ -0,0 +1,12 @@
+import "./style.css";
+// import { Link } from 'react-router-dom';
+
+function NotFound(props) {
+  return (
+    <div className="not--found">
+      <h1>Page Not Found</h1>
+    </div>
+  );
+}
+
+export default NotFound;
