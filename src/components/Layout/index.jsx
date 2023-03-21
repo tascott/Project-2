97955daa3fc4d4d2f33987616934ab646166d849
TomScott <contact@tascott.co.uk>
2023-03-21 16:23:59 +0000

    Layout compnent + footer

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "../../components/Header";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer";
+import "../../scss/style.css";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="layout--container">
+      <div className="layout--container__navbar">
+        <Header />
+        <Navbar />
+      </div>
+      <main>{children}</main>
+      <div className="layout--container__footer">
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
