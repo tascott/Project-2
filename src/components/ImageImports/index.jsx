be27520130b810d409cc1d06493486e5d5c1c015
TomScott <contact@tascott.co.uk>
2023-03-22 13:08:02 +0000

    Add images to projects page

---
+++ src/components/ImageImports/index.jsx
@@ -0,0 +1,17 @@
+import abc from '../../assets/images/abc.png';
+import xyz from '../../assets/images/xyz.png';
+import web123 from '../../assets/images/web123.webp';
+import acme from '../../assets/images/acme.png';
+import daily from '../../assets/images/daily.jpg';
+import fitness from '../../assets/images/fitness.jpg';
+import green from '../../assets/images/green.png';
+
+export const images = {
+    abc,
+    xyz,
+    web123,
+    acme,
+    daily,
+    fitness,
+    green,
+};
--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -1,9 +1,14 @@
 import "../../scss/style.css";
+import abc from '../../assets/images/abc.png';
+import { images } from "../../components/ImageImports";
 
 function ProjectCard(props) {
 
+  const imgSrc = images[props.image];
+
   return (
     <div className="project-card">
+      <img className="project--card_img" src={imgSrc} alt={props.website_name} />
       <h1 className="client">{props.client_name} <span className="location">{props.client_location}</span></h1>
       <h3 className="name">{props.website_name}</h3>
       <h4 className="cost">Estimated Cost: ${props.estimated_costs}</h4>
